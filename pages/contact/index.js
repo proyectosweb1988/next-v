f0c6aec56e91993b4fb29b32429dc321aa5a8438
Roberto Ontiveros <proyectosweb1988@gmail.com>
2022-03-05 04:10:13 -0600

    Reparando error de r

--- pages/contact/index.js
+++ pages/contact/index.js
@@ -1,8 +1,4 @@
-import Head from 'next/head'
-
-import { NavBar } from '../../components/NavBar'
-import Link from 'next/link'
-import styles from '../../styles/Home.module.css'
+import Link from 'next/link';
 import { MainLayouts } from '../../components/layouts/MainLayouts'
 
 export default function Contact() {
